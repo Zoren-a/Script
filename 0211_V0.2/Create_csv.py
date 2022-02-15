f236ea6d59704ae6b0d31cd739412d8ffd7516c1
Zoren-a <61437760+Zoren-a@users.noreply.github.com>
2022-02-15 13:11:56 -0600

    Delete 0211_V0.2 directory

--- 0211_V0.2/Create_csv.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-fields = ['SM', 'MEM', 'Shader_reg', 'Shared_mem', 'DL1', 'DL2']
-
-with open('Colelcted_data.csv', 'w') as csvfile:
-    csvwriter = csv.writer(csvfile)
-    csvwriter.writerow(fields)
